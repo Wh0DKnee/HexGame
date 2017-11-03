df815d021f05d38187d9d6b5ffa772d99ade19d9
Simon Reuter <s.reuter1@stud.tu-darmstadt.de>
2017-11-03 15:04:37 +0100

    before trying to implement network spawning

--- Assets/Scripts/Networking/PlayerInfo.cs
+++ Assets/Scripts/Networking/PlayerInfo.cs
@@ -8,10 +8,25 @@ namespace NetworkingCommonLib {
     public class PlayerInfo {
         public string nickname;
         //TODO: DONT HARDCODE
-        public string[] championNames = new string[]{"BasicChampion", "BasicChampion", "BasicChampion"};
+        public ChampionPosition[] championNames = new ChampionPosition[]{
+            new ChampionPosition("BasicChampion", HexCoordinates.CreateInstance(2, -2, 0)),
+            new ChampionPosition("BasicChampion", HexCoordinates.CreateInstance(2, -1, -1)),
+            new ChampionPosition("BasicChampion", HexCoordinates.CreateInstance(1, -2, 1))
+        };
 
         public PlayerInfo(string nickname) {
             this.nickname = nickname;
+        }
+    }
+
+    [System.Serializable]
+    public class ChampionPosition {
+        public string ChampionName { get; set; }
+        public HexCoordinates Coordinates { get; set; }
+
+        public ChampionPosition(string championName, HexCoordinates coordinates) {
+            ChampionName = championName;
+            Coordinates = coordinates;
         }
     }
 }
